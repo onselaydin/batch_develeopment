aa41a64fd2117cf09139a946493513bee9a2c909
Onsel <onselaydin@gmail.com>
2018-01-02 11:07:20 +0300

    inserted end and exit

--- devstopstart.bat
+++ devstopstart.bat
@@ -6,9 +6,9 @@ ECHO %date% TARIHLI ISLEMI YAPILACAK!
 ECHO LUTFEN DEVAM EDIN...
 ECHO.
 REM **************************************************
-SET var=Hello, World!
-net start | grep -x "OracleServiceXE"
-
+REM SET var=Hello, World!
+REM net start | grep -x "OracleServiceXE"
+sc query "OracleServiceXE" | find "RUNNING"
 REM if %ERRORLEVEL% == 2 goto trouble
 if %ERRORLEVEL% == 1 goto stopped
 if %ERRORLEVEL% == 0 goto started
@@ -18,21 +18,27 @@ ECHO !!!!!! SERVISLER DURDURULUYOR !!!!!!
 net stop lfsvc
 net stop OracleServiceXE
 net stop OracleXETNSListener
+net stop OracleOraClient12Home1_32bitMTSRecoveryService
 net stop MSSQL$SQLEXPRESS
 net stop SQLTELEMETRY$SQLEXPRESS
 net stop SQLWriter
 net stop YandexBrowserService
-net stop SQLAgent$SQLEXPRESS
-net stop SQLBrowser
-
+rem net stop SQLAgent$SQLEXPRESS
+rem net stop SQLBrowser
+goto end
 :stopped
 ECHO !!!!!! SERVISLER BAŞLATILIYOR !!!!!!
 net start lfsvc
 net start OracleServiceXE
 net start OracleXETNSListener
+net start OracleOraClient12Home1_32bitMTSRecoveryService
 net start MSSQL$SQLEXPRESS
 net start SQLTELEMETRY$SQLEXPRESS
 net start SQLWriter
 net start YandexBrowserService
-net start SQLAgent$SQLEXPRESS
-net start SQLBrowser
+rem net start SQLAgent$SQLEXPRESS
+rem net start SQLBrowser
+goto end
+:end
+ECHO ISLEM TAMAMLANDI.
+EXIT
